2fad6c3d3b65e402c8ab6c16f4d11836938f0bae
Howells <mrliuhaojie@163.com>
2024-11-06 09:45:53 +0800

    init

---
+++ src/pages/List.tsx
@@ -0,0 +1,63 @@
+import React, { FC, useState } from "react";
+import styles from "./List.module.scss";
+import QuestionCard from "../components/QuestionCard";
+const rawQuestionList = [
+  {
+    _id: "q1",
+    title: "问卷1",
+    isPublished: false,
+    isStar: false,
+    answerCount: 5,
+    createAt: "3月10日 13:56",
+  },
+  {
+    _id: "q2",
+    title: "问卷2",
+    isPublished: false,
+    isStar: true,
+    answerCount: 9,
+    createAt: "3月14日 13:56",
+  },
+  {
+    _id: "q3",
+    title: "问卷3",
+    isPublished: true,
+    isStar: false,
+    answerCount: 23,
+    createAt: "3月17日 13:56",
+  },
+  {
+    _id: "q4",
+    title: "问卷4",
+    isPublished: false,
+    isStar: false,
+    answerCount: 5,
+    createAt: "3月23日 16:56",
+  },
+];
+const List: FC = () => {
+  const [questionList, setQuestionList] = useState(rawQuestionList);
+  return (
+    <>
+      <div className={styles.header}>
+        <div className={styles.left}>
+            <h3>我的问卷</h3>
+        </div>
+        <div className={styles.right}>
+            (搜索)
+        </div>
+      </div>
+      
+      <div className={styles.content}>
+        {questionList.map(q=>{
+            const {_id} = q
+            return <QuestionCard key={_id} {...q} />
+        })}
+      </div>
+      <div className={styles.footer}>
+        底部
+      </div>
+    </>
+  );
+};
+export default List;
